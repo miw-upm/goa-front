db6365202287872cba5cda44e3c3de15405b354d
JBernal <j.bernal@upm.es>
2025-05-19 12:28:52 +0200

    prepared to cd

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,6 +4,5 @@ export const environment = {
     production: true,
     NAME: pkg.name,
     VERSION: pkg.version,
-    REST_USER: 'https://betca-tpv-user.herokuapp.com',
-    REST_CORE: 'https://betca-tpv-core.herokuapp.com'
+    REST_USER: 'https://gestion.ocanabogados.es/api',
 };
