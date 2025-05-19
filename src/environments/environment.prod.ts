71ca61b2b11c8aa3f379f3a094c83b01aee19122
JBernal <j.bernal@upm.es>
2025-05-19 15:23:48 +0200

    fixed REST_USER

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,6 +4,6 @@ export const environment = {
     production: true,
     NAME: pkg.name,
     VERSION: pkg.version,
-    REST_USER: 'https://gestion.ocanabogados.es/api',
+    REST_USER: 'https://gestion.ocanabogados.es/api/goa-user',
     FRONT_END: 'httpS://gestion.ocanabogados.es'
 };
