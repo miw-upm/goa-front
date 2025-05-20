62e466fcbf6452b92994eafb4adac8d0b5f40e32
JBernal <j.bernal@upm.es>
2025-05-20 07:00:55 +0200

    fixed secureRoutes  4

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,6 +5,6 @@ export const environment = {
     NAME: pkg.name,
     VERSION: pkg.version,
     REST_USER: 'https://gestion.ocanabogados.es/api/goa-user',
-    FRONT_END: 'httpS://gestion.ocanabogados.es',
+    FRONT_END: 'https://gestion.ocanabogados.es',
     SECURE_ROUTES: ['https://gestion.ocanabogados.es/api']
 };
