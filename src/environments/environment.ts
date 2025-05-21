92851bd6498128d63af80ba0012ffed9ba612020
JBernal <j.bernal@upm.es>
2025-05-21 12:11:48 +0200

    changed 'http://localhost:8080/api/goa-user'

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@ export const environment = {
     production: false,
     NAME: pkg.name,
     VERSION: pkg.version,
-    REST_USER: 'http://localhost:8080/goa-user',
+    REST_USER: 'http://localhost:8080/api/goa-user',
     FRONT_END: 'http://localhost:4200',
     SECURE_ROUTES: ['http://localhost:8080'],
 };
