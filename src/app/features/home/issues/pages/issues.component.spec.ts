2d2112fee07a5207d55c5806e6804130e4d3b399
Matheus Vieira Santos <matheus.eu.mv@gmail.com>
2026-03-17 00:12:44 +0100

    Add test coverage to front end miw-upm/goa#38

---
+++ src/app/features/home/issues/pages/issues.component.spec.ts
@@ -0,0 +1,31 @@
+import {Router} from '@angular/router';
+
+import {IssuesComponent} from './issues.component';
+
+describe('IssuesComponent', () => {
+    let routerSpy: { navigate: jasmine.Spy };
+
+    beforeEach(() => {
+        routerSpy = {
+            navigate: jasmine.createSpy('navigate').and.returnValue(Promise.resolve(true))
+        };
+    });
+
+    it('should navigate to issue detail with trimmed id', () => {
+        const component = new IssuesComponent(routerSpy as unknown as Router);
+        component.issueId = '  abc-123  ';
+
+        component.goToDetail();
+
+        expect(routerSpy.navigate).toHaveBeenCalledWith(['/home/issues', 'abc-123']);
+    });
+
+    it('should not navigate when id is empty', () => {
+        const component = new IssuesComponent(routerSpy as unknown as Router);
+        component.issueId = '   ';
+
+        component.goToDetail();
+
+        expect(routerSpy.navigate).not.toHaveBeenCalled();
+    });
+});
