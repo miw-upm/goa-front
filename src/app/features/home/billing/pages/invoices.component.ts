a050fcb2c1bd5f65d35c22950261fbd44d245eab
pablorojasfernandez <pablo.rojas.fernandez@alumnos.upm.es>
2026-03-23 20:29:40 +0100

    Added Invoices menu on billing miw-upm/goa#22

---
+++ src/app/features/home/billing/pages/invoices.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+    standalone: true,
+    selector: 'app-invoices',
+    template: `<p>Listado de facturas</p>`
+})
+export class InvoicesComponent {}
