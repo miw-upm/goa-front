48844559cad990df1531095739adb14181fcbfdd
pablorojasfernandez <pablo.rojas.fernandez@alumnos.upm.es>
2026-03-24 11:03:37 +0100

    Added tests to searchAll method miw-upm/goa#22

--- src/app/features/home/billing/pages/expenses.component.spec.ts
+++ src/app/features/home/billing/pages/expenses.component.spec.ts
@@ -2,6 +2,7 @@ import {of} from 'rxjs';
 
 import {ExpenseCreationDialogComponent} from '../dialogs/expense-creation-dialog.component';
 import {ExpensesComponent} from './expenses.component';
+import {Expense} from '../models/expense.model';
 
 describe('ExpensesComponent', () => {
     let dialogSpy: {
@@ -17,7 +18,9 @@ describe('ExpensesComponent', () => {
             })
         };
 
-        expenseServiceSpy = {};
+        expenseServiceSpy = {
+            search: jasmine.createSpy('search').and.returnValue(of([]))
+        };
     });
 
     it('should initialize title and empty list', (done) => {
@@ -48,5 +51,32 @@ describe('ExpensesComponent', () => {
         const dialogRef = dialogSpy.open.calls.mostRecent().returnValue;
         expect(dialogRef.afterClosed).toHaveBeenCalled();
         expect(openedDialog).toBeDefined();
+    });
+
+    it('should call search on expense service', () => {
+        const component = new ExpensesComponent(dialogSpy as any, expenseServiceSpy as any);
+
+        component.search();
+
+        expect((expenseServiceSpy as any).search).toHaveBeenCalled();
+    });
+
+    it('should update expenses on search', (done) => {
+                const expectedExpenses: Expense[] = [{
+            id: '11111111-1111-1111-1111-111111111111',
+            engagementId: '22222222-2222-2222-2222-222222222222',
+            amount: 100,
+                        date: '2026-03-24',
+            description: 'Test Expense'
+        }];
+        (expenseServiceSpy as any).search.and.returnValue(of(expectedExpenses));
+        const component = new ExpensesComponent(dialogSpy as any, expenseServiceSpy as any);
+
+        component.search();
+
+        component.expenses.subscribe(items => {
+            expect(items).toEqual(expectedExpenses);
+            done();
+        });
     });
 });
