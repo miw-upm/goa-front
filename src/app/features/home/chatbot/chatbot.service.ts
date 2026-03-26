baeadc1d64a4ca89ab1f4b07b2ab0373e1958bfe
Randy Mendez <randy.mendez@alumnos.upm.es>
2026-03-26 02:02:47 +0100

    Create chatbot service miw-upm/goa#101

---
+++ src/app/features/home/chatbot/chatbot.service.ts
@@ -0,0 +1,19 @@
+import {Injectable} from "@angular/core";
+import {Observable} from "rxjs";
+
+import {HttpService} from "@core/http/http.service";
+import {ChatbotMessageRequest, ChatbotMessageResponse} from "./models/chatbot.model";
+import {ENDPOINTS} from "@core/api/endpoints";
+
+@Injectable({ providedIn: 'root' })
+export class ChatbotService {
+    constructor(private readonly httpService: HttpService) {
+
+    }
+
+    sendMessage(request: ChatbotMessageRequest): Observable<ChatbotMessageResponse> {
+        return this.httpService.request()
+            .error('No se pudo obtener respuesta del asistente.')
+            .post(ENDPOINTS.chatbot.messages(), request);
+    }
+}
