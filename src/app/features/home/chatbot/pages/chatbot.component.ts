e0ea5590f006a74652eefd629d7ec70717eca961
jacardenas <juanancardenas@gmail.com>
2026-04-26 21:39:35 +0200

    enhanced button as icon miw-upm/goa#152

--- src/app/features/home/chatbot/pages/chatbot.component.ts
+++ src/app/features/home/chatbot/pages/chatbot.component.ts
@@ -6,6 +6,7 @@ import {MatInputModule} from "@angular/material/input";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatIconModule} from "@angular/material/icon";
 import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
+import {MatTooltipModule} from "@angular/material/tooltip";
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
 import {RouterLink} from "@angular/router";
 import {AuthService} from "@core/auth/auth.service";
@@ -32,6 +33,7 @@ import {TextFieldModule} from "@angular/cdk/text-field";
         MatFormFieldModule,
         MatIconModule,
         MatProgressSpinnerModule,
+        MatTooltipModule,
         TextFieldModule,
         RouterLink
     ],
