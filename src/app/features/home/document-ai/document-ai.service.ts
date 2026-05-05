2564b2b077e39996aac4036e389c43cf489aadcb
Yeri Jimenez Via y Rada <yeri.jimenez@autofact.cl>
2026-05-05 14:48:33 +0200

    ai document classification option added miw-upm/goa#63

--- src/app/features/home/document-ai/document-ai.service.ts
+++ src/app/features/home/document-ai/document-ai.service.ts
@@ -9,9 +9,10 @@ import {ENDPOINTS} from '@core/api/endpoints';
 export class DocumentAiService {
   constructor(private readonly httpService: HttpService) {}
 
-  uploadDocument(file: File): Observable<any> {
+  uploadDocument(file: File, autoclassify: boolean): Observable<any> {
     const formData = new FormData();
     formData.append('file', file);
+    formData.append('autoclassify', String(autoclassify));
     return this.httpService.request()
       .success('Documento subido con éxito')
       .error('Error al subir el documento')
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,7 @@ import pkg from '../../package.json';
 
 const HOST = window.location.origin;
 const API = `${HOST}/api`;
+const AI_DOCUMENT = 'https://x9dmu3rkbu.eu-west-1.awsapprunner.com'
 
 export const environment = {
     production: true,
@@ -14,5 +15,6 @@ export const environment = {
     REST_SUPPORT: `${API}/goa-support`,
     REST_DOCUMENT: `${API}/goa-document`,
     REST_CHATBOT: `${API}/goa-chatbot`,
-    SECURE_ROUTES: [API, `${API}/goa-chatbot`]
+    REST_AI_DOCUMENT: AI_DOCUMENT,
+    SECURE_ROUTES: [API, `${API}/goa-chatbot`, AI_DOCUMENT]
 };
