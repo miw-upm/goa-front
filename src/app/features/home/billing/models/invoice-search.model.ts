2e6e2854abc3ed1ce972cc51e5f0edcccee5ba90
JBernal <j.bernal@upm.es>
2026-05-24 02:24:31 +0200

    removed billing. miw-upm/goa#164

--- src/app/features/home/billing/models/invoice-search.model.ts
+++
@@ -1,4 +0,0 @@
-export interface InvoiceSearch {
-    engagementId?: string;
-    date?: string;
-}
--- src/app/features/home/billing/models/invoice.model.ts
+++
@@ -1,7 +0,0 @@
-export interface Invoice {
-    id: string;
-    engagementId: string;
-    date: string;
-    expenses: any[];
-    incomes: any[];
-}
